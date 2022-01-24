4102c467574dac415b1a80413cffcf663000909a
Rida R Syed <ridarafisyed@yahoo.com>
2022-01-24 18:40:36 +0500

    updating setting db

--- backend/settings.py
+++ backend/settings.py
@@ -95,13 +95,13 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'dextera_db',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'password1234',
-    #     'HOST': 'localhost'
-    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dextera_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password1234',
+        'HOST': 'localhost'
+    },
 }
 
 
