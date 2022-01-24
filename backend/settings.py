8135cebde69a8b18d108fe5f61c01306a379d434
Rida R Syed <ridarafisyed@yahoo.com>
2022-01-24 17:58:17 +0500

    updating setting

--- backend/settings.py
+++ backend/settings.py
@@ -91,17 +91,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'dextera_db',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'password1234',
-    #     'HOST': 'localhost'
-    # },
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dextera_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password1234',
+        'HOST': 'localhost'
+    },
 }
 
 
