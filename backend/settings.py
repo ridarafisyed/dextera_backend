17467a19bb1f1d2cb01a55e842eadb4963ec44f7
Rida R Syed <ridarafisyed@yahoo.com>
2022-01-24 13:34:35 +0500

    updating requirment

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'test_without_migrations',
     'rest_framework',
     'corsheaders',
     'knox',
