ed823161884e4f09db6a01a70263d57efa8fbd17
Rida R Syed <ridarafisyed@yahoo.com>
2022-04-30 13:57:24 +0500

    update grayout functionality

---
+++ build/precache-manifest.7d8d6aa89a9970acabfcde971b76e0a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15dc9347881ba8b2ada",
+    "url": "/static/css/main.25cebfa7.chunk.css"
+  },
+  {
+    "revision": "b15dc9347881ba8b2ada",
+    "url": "/static/js/main.b15dc934.chunk.js"
+  },
+  {
+    "revision": "fbe23182a92ba6add1e2",
+    "url": "/static/js/1.fbe23182.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa4704289c02b7f3f330fbd81f6b2fdc",
+    "url": "/static/media/logo_white copy.aa470428.png"
+  },
+  {
+    "revision": "a12d3677cf1b8040ffd02cb78d29b7de",
+    "url": "/static/media/login_light.a12d3677.png"
+  },
+  {
+    "revision": "181ee8a6ca15b37a1e56a361e0ae8d30",
+    "url": "/static/media/signup_light.181ee8a6.png"
+  },
+  {
+    "revision": "b3ada6fb05ba71d5133f32afb666c78d",
+    "url": "/index.html"
+  }
+];
