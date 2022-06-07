36f894a8ce1146a7dff0ccd41acdd30e101cfea6
Rida R Syed <ridarafisyed@yahoo.com>
2022-06-07 17:39:30 +0500

    adding CSRF settimgs

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3jsxxav7$cg!=!k-5=esmio-&+_+2@s*shq&)wnlx250!wy)!^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['*','https://dextera-lawfirm.netlify.app']
 
 
 # Application definition
@@ -164,7 +164,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://dextera-lawfirm.netlify.app",
+    "https://dextera-lawfirm.herokuapp.com",
+]
+CSRF_COOKIE_HTTPONLY = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
