8b265f2590438b7e2661d538a2782e0cdba5e2ca
Rida R Syed <ridarafisyed@yahoo.com>
2022-08-04 00:49:12 +0500

    updating setting

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from datetime import timedelta
 import django_on_heroku
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,7 +118,8 @@ DATABASES = {
         'HOST': 'localhost'
     },
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
